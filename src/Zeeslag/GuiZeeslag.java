ef6ec4efbfa87cbac0ca05778d543c985d5fb8d2
titaniumtank166 <35640970+titaniumtank166@users.noreply.github.com>
2020-06-08 11:10:16 +0200

    added gui
    adde host join gui

--- src/Zeeslag/GuiZeeslag.java
+++ src/Zeeslag/GuiZeeslag.java
@@ -1,11 +1,31 @@
 package Zeeslag;
-
+import javafx.scene.*;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
 public class GuiZeeslag
 {
+    private final Button buttonHostGame,buttonJoinGame;
+
     public GuiZeeslag(GridPane root)
     {
+        buttonHostGame = new Button("host game");
+        buttonJoinGame = new Button("join Game");
+
+        buttonJoinGame.setOnAction(event ->{
+
+
+
+        });
+
+        buttonHostGame.setOnAction(event -> {
+
+
+        });
+
+
+        root.add(buttonHostGame,3,1);
+        root.add(buttonJoinGame,3,2);
 
     }
 }
