a8be43239017761e3577eb16f379baeb9a6d894f
ChiekChiek <46634132+ChiekChiek@users.noreply.github.com>
2020-06-11 11:47:27 +0200

    Aangepaste data voor database
    wat test data wat getal uit test haald in database (wat alleen  een ID genereert)

--- src/Zeeslag/DatabaseConnector.java
+++ src/Zeeslag/DatabaseConnector.java
@@ -2,7 +2,7 @@ package Zeeslag;
 
 import java.sql.*;
 
-Public class DatabaseConnector
+public class DatabaseConnector
 {
 
     public void runDatabase(String args[])
@@ -12,14 +12,15 @@ Public class DatabaseConnector
             // Class.forName("com.mysql.cj.jdbc.Driver");
             // here testdb is database name, dbuser is username and dbwachtwoord is password  
             Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/testdb", "dbuser", "dbww");
+                    "jdbc:mysql://cfranken.gc-webhosting.nl:2083//cfranken_ZeeslagDB", "cfranken_cfranken", "ChicoGraafschap");
             Statement stmt = con.createStatement();
-            
-            ResultSet rs = stmt.executeQuery("SELECT * FROM drank");
+
+            //Tabellen in de database bestaan nog niet
+            ResultSet rs = stmt.executeQuery("SELECT * FROM test");
             
             while (rs.next())
             {
-                System.out.println(rs.getInt(1) + "  " + rs.getString(2));
+                System.out.println(rs.getInt(1) + "  " /**+ rs.getString(2)*/);
             }
             con.close();
         }
